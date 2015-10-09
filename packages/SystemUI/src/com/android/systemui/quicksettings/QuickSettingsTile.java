6ff6a9f8e371da28a6ecd9b647ab4e78166bf6d7
Estiko <estiko@mediaqu.com>
2015-10-09 21:31:34 +0700

    QuickTiles: Flip off by default
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -162,7 +162,7 @@ public class QuickSettingsTile implements View.OnClickListener {
 
     public boolean isFlipTilesEnabled() {
         return (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUICK_SETTINGS_TILES_FLIP, 1) == 1);
+                Settings.System.QUICK_SETTINGS_TILES_FLIP, 0) == 1);
     }
 
     public void flipTile(int delay){
