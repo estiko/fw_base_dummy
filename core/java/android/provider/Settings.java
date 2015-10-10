f07e70056730be279c74902b7a47e47e30ec6b76
Estiko <estiko@mediaqu.com>
2015-10-10 19:09:13 +0700

    Frameworks: Advanced low battery indicator options (1/2)
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4055,6 +4055,19 @@ public final class Settings {
         public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
 
        /**
+         * Battery warning preferences
+         *
+         * 0 = show dialog + play sound (default)
+         * 1 = fire notification + play sound
+         * 2 = show dialog only
+         * 3 = fire notification only
+         * 4 = play sound only
+         * 5 = none
+         * @hide
+         */
+        public static final String POWER_UI_LOW_BATTERY_WARNING_POLICY = "power_ui_low_battery_warning_policy";
+
+        /**
          * Whether incomming call UI stays in background
          *
          * @hide
