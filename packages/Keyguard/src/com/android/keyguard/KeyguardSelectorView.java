24da65c890bbb9a1ea22010fb71c9714f1892d26
Estiko <estiko@mediaqu.com>
2015-10-13 10:34:41 +0700

    FWB: LockBeforeUnlock the new way (1/2)
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -285,6 +285,22 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         }
     }
 
+	final boolean lockBeforeUnlock = Settings.Secure.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.Secure.LOCK_BEFORE_UNLOCK, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        // bring emergency button on slider lockscreen
+        // to front when lockBeforeUnlock is enabled
+        // to make it clickable
+        if (mLockPatternUtils != null && mLockPatternUtils.isSecure() && lockBeforeUnlock) {
+            LinearLayout ecaContainer =
+                (LinearLayout) findViewById(R.id.keyguard_selector_fade_container);
+            if (ecaContainer != null) {
+                ecaContainer.bringToFront();
+            }
+        }
+
     public void setCarrierArea(View carrierArea) {
         mFadeView = carrierArea;
     }
@@ -576,9 +592,9 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     }
 
     public void updateLockscreenBattery(KeyguardUpdateMonitor.BatteryStatus status) {
-        if (Settings.PAC.getIntForUser(
+        if (Settings.System.getIntForUser(
                 mContext.getContentResolver(),
-                Settings.PAC.BATTERY_AROUND_LOCKSCREEN_RING,
+                Settings.System.BATTERY_AROUND_LOCKSCREEN_RING,
                 0 /*default */,
                 UserHandle.USER_CURRENT) == 1) {
             if (status != null) mBatteryLevel = status.level;
