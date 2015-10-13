20b841af0d6fd627d0ad1a66262c7f25fbcf1e3f
Estiko <estiko@mediaqu.com>
2015-10-13 10:22:10 +0700

    FWB: Navigation bar dimensions (1/2)
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4238,6 +4238,24 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_NOTIFICATIONS_COLOR = "lockscreen_notifications_color";
 
+	/**
+         * Wether navigation bar is enabled or not
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+	/**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
         /**
          * Animate-flip Quick Settings Panel Tiles on click
          *
