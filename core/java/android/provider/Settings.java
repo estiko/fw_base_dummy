3cc343297cbfa15b44a0f8886d440aa88c6c99c4
Estiko <estiko@mediaqu.com>
2015-10-15 20:59:01 +0700

    Oooppsss :D
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4122,6 +4122,12 @@ public final class Settings {
         public static final String RECENTS_RAM_BAR_ACTIVE_APPS_COLOR = "recents_ram_bar_active_apps_color";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Allows settings to blurring the lockscreen background
          * @hide
          */
