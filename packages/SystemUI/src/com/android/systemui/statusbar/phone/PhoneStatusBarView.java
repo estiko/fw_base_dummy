24c24e98a3334a2cfc66ce4ecdb63ce127e4089d
Estiko <estiko@mediaqu.com>
2015-10-16 23:29:04 +0700

    Remove Notification Reminders
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -178,8 +178,6 @@ public class PhoneStatusBarView extends PanelBar {
         if (DEBUG) {
             Log.v(TAG, "start opening: " + panel + " shouldfade=" + mShouldFade);
         }
-
-        mBar.toggleReminderFlipper(true);
         mFadingPanel = panel;
     }
 
