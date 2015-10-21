35ae1cd2c497aafa5d05ea49746a11e6c7145cea
Estiko <estiko@mediaqu.com>
2015-10-21 09:10:08 +0700

    Frameworks: go kitkat + style tweaks and follow googles color sheme
    Signed-off-by: Estiko <estiko@mediaqu.com>

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -11080,4 +11080,10 @@ public class WindowManagerService extends IWindowManager.Stub
     public void addSystemUIVisibilityFlag(int flag) {
         mLastStatusBarVisibility |= flag;
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return mLastStatusBarVisibility;
+    }
 }
